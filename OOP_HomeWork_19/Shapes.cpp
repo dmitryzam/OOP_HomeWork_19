78399bfe32f811fc5dba2f5385c604ea7e7bca15
dmitryzam <dmitryzam@mail.ru>
2024-12-04 08:24:15 +0500

    Add project files.

---
+++ OOP_HomeWork_19/Shapes.cpp
@@ -0,0 +1,144 @@
+﻿#include "Shapes.hpp"
+
+std::string square_file = "square_file";
+std::string rectangle_file = "rectangle_file";
+std::string circle_file = "circle_file";
+std::string ellipse_file = "ellipse_file";
+
+void Square::setParam(int x, int y, double sideLength){
+	coord_x = x;
+	coord_y = y;
+	side_length = sideLength;
+}
+
+void Square::show(){
+	std::cout << "Фигура - квадрат;\n";
+	std::cout << "Координаты: " << coord_x << ", " << coord_y << ";\n";
+	std::cout << "Длина стороны: " << side_length << " мм." << std::endl;
+}
+
+void Square::save(){
+	std::ofstream fout;
+	fout.open(square_file);
+	if (!fout.is_open()) {
+		std::cerr << "Warning! Can't open file " << square_file << " file for output.\n";
+		exit(EXIT_FAILURE);
+	}
+	fout.write((char*)this, sizeof(Square));	// данная ф-ия не работает с переменными string!!!!!@
+	fout.close();
+}
+
+void Square::load(){	
+	std::ifstream fin(square_file);
+	if (!fin.is_open()) {
+		std::cerr << "Warning! Can't open file " << square_file << " file for output.\n";
+		exit(EXIT_FAILURE);
+	}
+	fin.read((char*)this, sizeof(Square));	// данная ф-ия не работает с переменными string!!!!!@
+	fin.close();
+}
+
+void Rectangle::setParam(int x, int y, double length, double width){
+	coord_x = x;
+	coord_y = y;
+	this->length = length;
+	this->width = width;
+}
+
+void Rectangle::show(){
+	std::cout << "Фигура - прямоугольник;\n";
+	std::cout << "Координаты: " << coord_x << ", " << coord_y << ";\n";
+	std::cout << "Длина: " << length << " мм, высота: " << width << " мм." << std::endl;
+}
+
+void Rectangle::save(){
+	std::ofstream fout;
+	fout.open(rectangle_file);
+	if (!fout.is_open()) {
+		std::cerr << "Warning! Can't open file " << rectangle_file << " file for output.\n";
+		exit(EXIT_FAILURE);
+	}
+	fout.write((char*)this, sizeof(Rectangle));
+	fout.close();
+}
+
+void Rectangle::load(){
+	std::ifstream fin(rectangle_file);
+	if (!fin.is_open()) {
+		std::cerr << "Warning! Can't open file " << rectangle_file << " file for output.\n";
+		exit(EXIT_FAILURE);
+	}
+	fin.read((char*)this, sizeof(Rectangle));
+	fin.close();
+}
+
+void Circle::setParam(int x, int y, double radius){
+	coord_x = x;
+	coord_y = y;
+	this->radius = radius;
+}
+
+void Circle::show(){
+	std::cout << "Фигура - окружность;\n";
+	std::cout << "Координаты: " << coord_x << ", " << coord_y << ";\n";
+	std::cout << "Радиус: " << radius << " мм." << std::endl;
+}
+
+void Circle::save(){
+	std::ofstream fout(circle_file);
+	if (fout.is_open()) {
+		fout.write((char*)this, sizeof(Circle));
+		fout.close();
+	}
+	else {
+		std::cout << "Error! Can't open" << circle_file << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Circle::load(){
+	std::ifstream fin;
+	fin.open(circle_file);
+	if (!fin.is_open()) {
+		std::cerr << "Error! Can't open file!\n";
+		exit(EXIT_FAILURE);
+	}
+	fin.read((char*)this, sizeof(Circle));
+	fin.close();
+}
+
+void Ellipse::setParam(int x, int y, double length, double width){
+	coord_x = x;
+	coord_y = y;
+	this->length = length;
+	this->width = width;
+}
+
+void Ellipse::show(){
+	std::cout << "Фигура - эллипс;\n";
+	std::cout << "Координаты: " << coord_x << ", " << coord_y << ";\n";
+	std::cout << "Длина: " << length << " мм, высота: " << width << " мм прямоугольника, кот. описывает эллипс." << std::endl;
+}
+
+void Ellipse::save(){
+	std::ofstream fout;
+	fout.open(ellipse_file);
+	if (!fout.is_open()) {
+		std::cerr << "Error! Can't open " << ellipse_file << " file!\n";
+		exit(EXIT_FAILURE);
+	}
+	fout.write((char*)this, sizeof(Ellipse));
+	fout.close();
+}
+
+void Ellipse::load(){
+	std::ifstream fin(ellipse_file);
+	if (fin.is_open()) {
+		fin.read((char*)this, sizeof(Ellipse));
+		fin.close();
+	}
+	else {
+		std::cerr << "Error! Can't open file!\n";
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ OOP_HomeWork_19/Shapes.hpp
@@ -0,0 +1,63 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstdlib> // для ф-ии exit()
+
+class Shape {
+public:	
+	// вывод на экран информации о фигуре:
+	virtual void show() = 0;
+	// сохранение фигуры в файл:
+	virtual void save() = 0;
+	// считывание фигуры из файла:
+	virtual void load() = 0;	
+};
+// класс Квадрат:
+class Square : public Shape {
+private:
+	int coord_x, coord_y;
+	double side_length;
+public:
+	Square() : coord_x(0), coord_y(0), side_length(1) {}
+	void setParam(int x, int y, double sideLength);	
+	virtual void show() override;
+	virtual void save() override;
+	virtual void load() override;
+};
+// класс Прямоугольник:
+class Rectangle : public Shape {
+private:
+	int coord_x, coord_y;
+	double length, width;
+public:
+	Rectangle() : coord_x(0), coord_y(0), length(1.), width(1.){}
+	void setParam(int x, int y, double length, double width);
+	virtual void show() override;
+	virtual void save() override;
+	virtual void load() override;
+};
+// класс Окружность:
+class Circle : public Shape {
+private:
+	int coord_x, coord_y;
+	double radius;
+public:
+	Circle() : coord_x(0), coord_y(0), radius(1.) {}
+	void setParam(int x, int y, double radius);
+	virtual void show() override;
+	virtual void save() override;
+	virtual void load() override;
+};
+// класс Эллипс:
+class Ellipse : public Shape {
+private:
+	int coord_x, coord_y;	// координаты верхнего-левого угла прямоугольника, кот. описывает эллипс
+	double length, width;	// размеры этого прямоугольника
+public:
+	Ellipse() : coord_x(0), coord_y(0), length(1.), width(1.){}	
+	void setParam(int x, int y, double length, double width);
+	virtual void show() override;
+	virtual void save() override;
+	virtual void load() override;
+};
